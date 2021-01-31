d0e854b1ab9731f447eb2d515906850d563cc76c
Charles Hall <chall8001@gmail.com>
2021-01-30 21:00:26 -0700

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("test")
+
 
 //create a function that displays the current date on the top of the page
 //create a loop function that generate 24 time blocks for each hour in the day
@@ -8,22 +8,21 @@ console.log("test")
 
 //code snippet for the current date and time object 
 var d = new Date();
-//select all the different time blocks
-//use .each jquery function-- use this 
-//inside of there compare the current time to that rows time 
 
+
+//function to execute jquery after the page loads 
 $(document).ready(function(){
 
+//accsess the save button on click, and the value in the text area
 $('.saveBtn').on('click', function(){
 var desEl = $(this).siblings(".description").val()
 var desKey =  $(this).siblings(".hour").text()
-console.log(desKey)
-console.log(desEl)
-
+//save the text to local storage as a key value pair
 localStorage.setItem(desKey, desEl)
 
 })
 
+//get the pair from local storage
 $("#9 .description").val(localStorage.getItem("9:00 am"))
 $("#10 .description").val(localStorage.getItem("10:00 am"))
 $("#11 .description").val(localStorage.getItem("11:00 am"))
@@ -34,7 +33,33 @@ $("#3 .description").val(localStorage.getItem("3:00 pm"))
 $("#4 .description").val(localStorage.getItem("4:00 pm"))
 $("#4 .description").val(localStorage.getItem("5:00 pm"))
 
+//print the current time in the header
+
 var currentTime = $('#currentDay').text(d)
 console.log(currentTime)
+color()
 
-})
+//I commented out this next part because I couldn;t find the bug, and it was breaking my code, but it was suppossed to color the planner according to time
+
+// function color(){
+//     var k = new Date();
+//     var n = k.getHours();
+
+//     $(".hour").each(function(){
+//      if (n = $(this)) 
+//      {
+//          $(this).siblings(".description").add("present")
+      
+//     }
+
+//     else if (n > $(this)) {
+//         $(this).siblings(".description").addClas("past")
+//     }
+    
+//     else (n < $(this))
+//     {$(this).siblings(".description").addClass("future")
+// }
+
+    
+// }) 
+// }
